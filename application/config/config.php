75e2f0eb8818a382c381337bcbcb229287e8f272
GrenjoByte <grenjo.dev@gmail.com>
2026-05-04 19:03:48 +0800

    Fixed client grab while currently serving another

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.216.213/q_sys';
-// $config['base_url'] = 'http://localhost/q_sys';
+// $config['base_url'] = 'http://192.168.216.213/q_sys';
+$config['base_url'] = 'http://localhost/q_sys';
 
 /*
 |--------------------------------------------------------------------------
@@ -378,12 +378,12 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver']             = 'files';
-$config['sess_cookie_name']        = 'ci_session';
-$config['sess_expiration']         = 7200;
-$config['sess_save_path']          = APPPATH . 'sessions';
-$config['sess_match_ip']           = FALSE;
-$config['sess_time_to_update']     = 300;
+$config['sess_driver'] = 'files';
+$config['sess_cookie_name'] = 'ci_session';
+$config['sess_expiration'] = 7200;
+$config['sess_save_path'] = NULL;
+$config['sess_match_ip'] = FALSE;
+$config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
 
 /*
